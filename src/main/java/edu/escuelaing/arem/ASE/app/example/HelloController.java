d7b76214ed42be3a80d1c5394ad3f2891471de33
Daniel Santiago Gómez Zabala <123691122+sagomezab@users.noreply.github.com>
2024-02-24 12:09:50 -0500

    Update HelloController.java

--- src/main/java/edu/escuelaing/arem/ASE/app/example/HelloController.java
+++ src/main/java/edu/escuelaing/arem/ASE/app/example/HelloController.java
@@ -3,7 +3,7 @@ package edu.escuelaing.arem.ASE.app.example;
 @Component
 public class HelloController {
     
-    @GetMapping("/hello")
+    @GetMapping("/testing")
     public static String index(){
         return "Greetings from Spring Boot!";
     }
